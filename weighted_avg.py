fc57e04b6c9fdf078d34b63edf2461a3fc230ecf
Lamia45711704 <ahsanhabib3@iut-dhaka.edu>
2023-12-29 14:58:22 +0600

    requirements updated

---
+++ weighted_avg.py
@@ -0,0 +1,7 @@
+import pandas as pd
+
+df = pd.read_excel(r"../Data/Programmers' Survey on Question Comprehensibility of StackOverflow (Responses).xlsx")
+
+print(df)
+
+# Baki kaaj gula Khulna theke eshe kortesi. Till then baad rakhlam
